e3a1cc8171c1c2579ec18aa89b92b4968634cf41
Tiago Tresoldi <tiago.tresoldi@lingfil.uu.se>
2022-12-04 12:31:22 +0100

    Proceed with csvdeploy code import

--- src/tasyba/render.py
+++ src/tasyba/render.py
@@ -17,7 +17,7 @@ def load_template_env(template_path: str) -> Environment:
     Load a Jinja2 template environment.
     """
 
-    logging.debug("Loading Jinja2 template environment")
+    logging.info("Loading Jinja2 template environment")
 
     # Build template_file and layout path; note that Jinja2 documentation says
     # that template names are not filesystem paths (even though
@@ -25,6 +25,8 @@ def load_template_env(template_path: str) -> Environment:
     # even under Windows.
     template_path = Path(template_path).resolve()
     template_path = template_path.as_posix().replace("\\", "/")
+
+    print("----", template_path)
 
     template_env = Environment(loader=FileSystemLoader(template_path))
 
@@ -59,6 +61,62 @@ def build_html(template_env, replaces, tables, output_file, config):
     logging.info("`%s` wrote with %i bytes.", output_file, len(source))
 
 
+# TODO: write properly etc. should load with other templates;
+# TODO: also copy images if needed
+def build_css(template_env, replaces, config):
+    template = template_env.get_template("main.css")
+
+    source = template.render(**replaces)
+
+    # build and write css file
+    file_path = Path("main.css")
+    with open(file_path.as_posix(), "w") as handler:
+        handler.write(source)
+
+
+def build_tables(data, replaces, tables, template_env, config):
+    # TODO: fitting the data to the what is expected by the template
+    fields = config["single_table"]
+
+    rows = []
+    for row in data["single"]:
+        subrow = [{"value": row[field], "url": None} for field in fields]
+        rows.append(subrow)
+
+    table_data = {"columns": [{"name": field} for field in fields], "rows": rows}
+
+    for table in data:
+        table_replaces = replaces.copy()
+        table_replaces["datatable"] = table_data
+        build_html(template_env, table_replaces, tables, f"{table}.html", config)
+
+
+def build_sql_page(data, replaces, template_env, config):
+    # Compute inline data replacements
+    inline_data = {}
+    for table_name in data:
+        inline_data[table_name] = []
+        for row in data[table_name]["rows"]:
+            row_insert = ", ".join(
+                ["'%s'" % cell["value"] if cell["value"] else "NULL" for cell in row]
+            )
+            inline_data[table_name].append(row_insert)
+
+    # Build table schemata
+    # TODO: use datatypes
+    schemata = {}
+    for table_name in data:
+        schemata[table_name] = ", ".join(
+            ["%s text" % col["name"].lower() for col in data[table_name]["columns"]]
+        )
+
+    # Generate page
+    sql_replaces = replaces.copy()
+    sql_replaces["data"] = inline_data
+    sql_replaces["schemata"] = schemata
+    build_html(template_env, sql_replaces, "sql.html", config)
+
+
 def render_database(data, replaces, tables, config):
     """
     Render the database to HTML.
@@ -68,10 +126,19 @@ def render_database(data, replaces, tables, config):
     # TODO: allow to specify a different template path
     template_path = None
     if not template_path:
-        template_path = Path(__file__).parent / "template" / "default"
+        template_path = Path(__file__).parent.parent.parent / "templates" / "default"
 
     # Load Jinja HTML template environment
     template_env = load_template_env(template_path)
 
     # Build and write index.html
     build_html(template_env, replaces, tables, "index.html", config)
+
+    # Build CSS files from template
+    build_css(template_env, replaces, config)
+
+    # Build tables from CLDF data
+    build_tables(data, replaces, tables, template_env, config)
+
+    # Build SQL query page
+    # build_sql_page(data, replaces, template_env, config)
